f1144ad7bd77cc3fd622a4a5517912c6fec1194e
Raphael Brunner <raphael@raphaelbrunner.ch>
2016-04-13 15:24:56 +0200

    Menu with some buttons, but without logic

---
+++ thesis-run/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject settingsPanel;
+
+	public void Settings(){
+		settingsPanel.SetActive (true);
+	}
+
+	public void SettingsClose() {
+		settingsPanel.SetActive (false);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

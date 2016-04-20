6258a9a3643200181106e712054cb065847ffaef
Raphael Brunner <raphael@raphaelbrunner.ch>
2016-04-20 16:26:06 +0200

    infinity floor

---
+++ thesis-run/Assets/Scripts/BackgroundImages.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundImages : MonoBehaviour {
+
+
+	public Vector2 velocity = new Vector2(0, -4);
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = velocity; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -12) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ thesis-run/Assets/Scripts/FloorScroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorScroller : MonoBehaviour {
+
+	public float speed = 0.2f;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (0, Time.time * speed);
+
+		rend.material.mainTextureOffset = offset;
+	}
+}

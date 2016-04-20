f221169d68601986e9a492a657e9300d780019eb
Raphael Brunner <raphael@raphaelbrunner.ch>
2016-04-20 16:35:15 +0200

    infinity Propertys on background

--- thesis-run/Assets/Scripts/BackgroundGenerator.cs
+++ thesis-run/Assets/Scripts/BackgroundGenerator.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class BackgroundGenerator : MonoBehaviour {
 
+	public GameObject pillar;
+	public GameObject table;
+
 	// Use this for initialization
 	void Start () {
-	
+		InvokeRepeating ("CreatePillar", 0f, 1f);
+		InvokeRepeating ("CreateTable", 0f, 0.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	void CreatePillar(){
+		Instantiate(pillar);
+	}
+
+	void CreateTable(){
+		Instantiate(table);
 	}
 }

569836906a14d5d0cf38209d43fa0861094cf308
Marius Küng <maku@mailbox.org>
2016-04-27 15:28:13 +0200

    Added progressbar logic
    Increase width incrementally

---
+++ thesis-run/Assets/Scripts/GameProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameProgressBar : MonoBehaviour {
+
+	public Image progressBar;
+	float time = 0f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("increaseTime", 1.0f, 1.0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (time == 60) {
+			// win
+			SceneManager.LoadScene ("menu");
+		}
+		progressBar.rectTransform.sizeDelta = new Vector2 (10 + (time * 4), 20f);
+	}
+
+	void increaseTime() {
+		this.time++;
+	}
+}

522c8b827a2eb5b46b470c52fdc8bb09932878c1
Raphael Brunner <raphael@raphaelbrunner.ch>
2016-05-04 15:53:00 +0200

    added initial game mechanics object

---
+++ thesis-run/Assets/Scripts/GameMechanic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMechanic : MonoBehaviour {
+
+	private float[] lanes = new float[3];
+
+	// Use this for initialization
+	void Start () {
+		var lanePos = Screen.width / (lanes.Length + 1);
+		for (var i = 0; i < lanes.Length; i++) {
+			lanes [i] = lanePos * (i + 1);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GameLost (){
+		
+	}
+
+	void GameWon (){
+		
+	}
+
+	void IncreaseTime () {
+	}
+
+	void DecreaseTime () {
+	}
+
+	float[] GetLanes() {
+		return lanes;
+	}
+}

9cabc74b6b7bc3b34e71041d2f8d20d79cf03e65
Marius Küng <maku@mailbox.org>
2016-05-26 15:09:54 +0200

    Added powerup and obstacle prefabs and itemgenerator with item lists

---
+++ thesis-run/Assets/Scripts/ItemGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemGenerator : MonoBehaviour {
+	
+	public List<GameObject> obstacles;
+	public List<GameObject> powerups;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CreateObstacle", 1f, 1.5f);
+		InvokeRepeating ("CreatePowerup", 2f, 3f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void CreateObstacle(){
+		var obstacle = obstacles [Random.Range (0, obstacles.Count)];
+		Instantiate(obstacle);
+	}
+
+	void CreatePowerup(){
+		var powerup = powerups [Random.Range (0, powerups.Count)];
+		Instantiate(powerup);
+	}
+}
--- thesis-run/Assets/Scripts/Obstacle.cs
+++ thesis-run/Assets/Scripts/Obstacle.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Item : MonoBehaviour {
+public class Obstacle : MonoBehaviour {
 
 	public GameMechanic mechanics;
 	public Vector2 velocity = new Vector2(0, -4);
@@ -16,17 +16,24 @@ public class Item : MonoBehaviour {
 			lanes [i] = pos * (i + 1);
 		}
 		lane = lanes [getLane ()];
-//		lane = lanes [1]; // for testing we positin the item in the center
+		//		lane = lanes [1]; // for testing we positin the item in the center
 		transform.position = Camera.main.ViewportToWorldPoint(new Vector3(lane, 1f, 10f));
 		GetComponent<Rigidbody2D> ().velocity = velocity;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		// when item leaves screen
 		if (transform.position.y < -6) {
 			Destroy (gameObject);
 		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		Debug.Log ("Obstacle collision with player!");
+		Destroy (gameObject);
+
+		// update progressBar here
 	}
 
 	int getLane(){
